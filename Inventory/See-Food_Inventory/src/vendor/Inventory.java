247ac0945ca172c9d3cf5d981caf5056a7494d72
redxzeta <xzetanzero@gmail.com>
2017-04-12 19:12:08 -0700

    Inventory

--- Inventory/See-Food_Inventory/src/vendor/Inventory.java
+++ Inventory/See-Food_Inventory/src/vendor/Inventory.java
@@ -1,5 +1,36 @@
 package vendor;
 
-public class Inventory {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+public class Inventory extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		BorderPane bp = new BorderPane();
+		MyPane mp = new MyPane();
+		bp.setCenter(mp);
+		Scene scene = new Scene (bp, 400,400);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("See-Food ");
+		primaryStage.show();
+		mp.requestFocus();
+		
+		
+	}
+	
+
+
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	Application.launch(args);
+}
+
+class MyPane extends Pane {
+	
+	
+}
 }
