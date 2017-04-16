78c82910831b4466485caf1e3d4f9567d511abf5
Rolf Castro <rcastr13@calstatela.edu>
2017-04-16 12:00:46 -0700

    Pushed SeeFoodController

--- SeeFoodWorkspace/See-Food/src/controllers/SeeFoodController.java
+++ SeeFoodWorkspace/See-Food/src/controllers/SeeFoodController.java
@@ -1,22 +1,25 @@
-package modules;
+package controllers;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class RegistrationServlet
- */
-@WebServlet("/RegistrationServlet")
-public class RegistrationServlet extends HttpServlet {
+@WebServlet("/SeeFoodController")
+public class SeeFoodController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		boolean logout = request.getParameter("logout") != null;
 		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		if(logout){
+			request.getSession().invalidate();
+		}
+		
+		request.getRequestDispatcher("/WEB-INF/SFSS/Homepage.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- SeeFoodWorkspace/See-Food/src/models/ProductBean.java
+++ SeeFoodWorkspace/See-Food/src/models/ProductBean.java
@@ -1,4 +1,4 @@
-package cs3337;
+package models;
 
 public class ProductBean {
 
