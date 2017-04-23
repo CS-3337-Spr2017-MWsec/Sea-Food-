c617f905c10e0d257119dda3644e1bc99b1e4729
Rolf Castro <rcastr13@calstatela.edu>
2017-04-22 19:52:48 -0700

    Add OrderController and OrderConfirmation.jsp.

---
+++ SeeFoodWorkspace/See-Food/src/controllers/OrderController.java
@@ -0,0 +1,46 @@
+package controllers;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.ProductBean;
+
+@WebServlet("/OrderController")
+public class OrderController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/SFSS/OrderConfirmation.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//TEST.
+//		@SuppressWarnings("unchecked")
+//		ArrayList<ProductBean> userProducts = (ArrayList<ProductBean>) getServletContext().getAttribute("userProducts");
+//		
+//		//TODO Test if these are actually required.
+//		Integer itemID = Integer.valueOf(request.getParameter("id"));
+//		Integer numOfItems = Integer.parseInt(request.getParameter("itemQuantity")); //User amount in ShoppingCart.jsp.
+////		String desc = request.getParameter("desc");
+//		
+//		for(ProductBean itemInCart: userProducts){
+//			if(itemID.equals(itemInCart.getId())){
+//				itemInCart.setQuantity(numOfItems);
+//			}
+//		}
+//		
+//		//TESTING.
+//		if(request.getParameter("username") == null){
+//			request.setAttribute("billing", true);
+//			response.sendRedirect("RegistrationController");
+//		}
+		
+		doGet(request, response);
+	}
+}
