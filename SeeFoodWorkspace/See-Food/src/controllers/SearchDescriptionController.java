df58d54e302155a537d9f68d2cb3de6f74171608
Rolf Castro <rcastr13@calstatela.edu>
2017-05-02 13:56:19 -0700

    Fix styling to controllers.

--- SeeFoodWorkspace/See-Food/src/controllers/SearchDescriptionController.java
+++ SeeFoodWorkspace/See-Food/src/controllers/SearchDescriptionController.java
@@ -20,7 +20,6 @@ import models.ProductBean;
 
 
 @WebServlet("/SearchDescriptionController")
-//@WebServlet("/Product/SearchDescription")
 public class SearchDescriptionController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
