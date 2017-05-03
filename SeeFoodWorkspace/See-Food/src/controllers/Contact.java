79b0acf39282cb0b0dd0920429df53c5f3d30978
redxzeta <xzetanzero@gmail.com>
2017-05-03 10:12:02 -0700

    added contact

---
+++ SeeFoodWorkspace/See-Food/src/controllers/Contact.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Contact
+ */
+@WebServlet("/Contact")
+public class Contact extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("/WEB-INF/SFSS/ContactMe.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
